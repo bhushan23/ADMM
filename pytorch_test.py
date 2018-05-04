35216dd5806c3f95a228c9f2f6cc7d1f7ef75123
bhushan <bhushan.s.94@gmail.com>
2018-05-04 18:57:37 -0400

    Objective functions Lasso

--- pytorch_test.py
+++ pytorch_test.py
@@ -2,17 +2,17 @@ import numpy as np
 import torch.optim as optim
 
 num_iterations = 5
-N = 1000
-D = 20
+N = 100000
+D = 200
 
 A = np.random.randn(N, D)
 b = np.random.randn(N, 1)
 # Y = np.random.rand(N, 1)
 
-admm = optim.ADMM([A, b, 0.01], "Lasso") #, parallel = True)
+admm = optim.ADMM([A, b, 0.01, 1], "Lasso") #, parallel = True)
 
 print(admm.getLoss())
 for i in range(0, num_iterations):
     print('O Val:', admm.step())
 
-print('Weights: ',admm.getWeights())
+print('Weights: ',admm.getParams())
